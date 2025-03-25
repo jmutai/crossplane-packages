755293af91cb1eb2503a792c58beabdffc3b3045
Josphat Mutai <josphatkmutai@gmail.com>
2025-03-25 21:52:40 +0300

    ceph bucket

--- install_crossplane_cli.sh
+++ install_crossplane_cli.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 curl -sL "https://raw.githubusercontent.com/crossplane/crossplane/main/install.sh" | sh
+
+# KCL
+## https://www.kcl-lang.io/docs/user_docs/getting-started/install
+curl -fsSL https://kcl-lang.io/script/install-cli.sh | /bin/bash
+## https://www.kcl-lang.io/docs/user_docs/getting-started/install#install-language-server
+curl -fsSL https://kcl-lang.io/script/install-kcl-lsp.sh | /bin/bash
